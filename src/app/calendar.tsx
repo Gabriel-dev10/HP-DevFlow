e7047caf42648f027e5da24c6f285bf15c557802
Gabriel Santana <gabrielssantana41@gmail.com>
2026-09-21 19:32:29 -0300

    chore: remover lixo

--- src/app/calendar.tsx
+++
@@ -1,9 +0,0 @@
-import { View, Text } from 'react-native';
-
-export default function CalendarScreen() {
-  return (
-    <View>
-      <Text>Agenda</Text>
-    </View>
-  );
-}
--- src/app/statistics.tsx
+++
@@ -1,9 +0,0 @@
-import { View, Text } from 'react-native';
-
-export default function StatisticsScreen() {
-  return (
-    <View>
-      <Text>Estatísticas</Text>
-    </View>
-  );
-}
